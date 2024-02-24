b637708cc3ecf9a6bea2109b7a62d29be51ffef5
tarkour <dov.raven@gmail.com>
2024-02-24 18:25:28 +0900

    deleted trash files

--- model/test_table.py
+++
@@ -1,9 +0,0 @@
-from . import BaseModel
-from sqlalchemy import Column, Integer, String, DateTime
-
-
-class TestTable(BaseModel):
-    __tablename__ = "test_table"
-
-    id = Column('id', Integer, primary_key=True)
-    user_id = Column('name', String)
