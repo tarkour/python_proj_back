fcb48a3fe2d9d0a90ec588c431f77dbebd6416f4
tarkour <dov.raven@gmail.com>
2024-02-25 17:45:38 +0900

    changed the way how to grab user_id(now grabs from model)

--- service/messages.py
+++ service/messages.py
@@ -7,8 +7,9 @@ class SendMessage():
         self.userRepository = userRepository
         self.messageRepository = messageRepository
 
-    def send_message(self,username, message):
-        user_id = self.userRepository.get_user_id(username)
+    def send_message(self,user, message):
+        user_model = self.userRepository.model
+        user_id = user_model.get_user_id()
         current_time = datetime.now(pytz.timezone('Europe/Moscow'))
         message_data = self.messageRepository.model(user_id, message, current_time)
 
